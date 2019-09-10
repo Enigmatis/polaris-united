d1e8ac663cb83f37636d910d3d6d0ceadee3617c
yarinvak <yarinvak@gmail.com>
2019-09-10 11:43:31 +0300

    update name in package json export graphql logger

--- packages/polaris-graphql-logger/src/main.ts
+++ packages/polaris-graphql-logger/src/main.ts
@@ -1,2 +1,3 @@
+export { GraphQLLogger } from './logger/graphql-logger';
 export {PolarisGraphQLLogger} from './logger/polaris-graphql-logger';
 export {GraphQLLogProperties} from './logger/graphql-log-properties';
