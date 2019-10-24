1d3d0293ee500f8b25811e4554302feaba32a1cc
chenshoo <chenshooker@gmail.com>
2019-10-24 15:03:37 +0300

    fix: fix export

--- packages/polaris-graphql-logger/src/main.ts
+++ packages/polaris-graphql-logger/src/main.ts
@@ -1,3 +1,3 @@
-export { GraphQLLogger } from './logger/graphql-logger';
+export {GraphQLLogger} from './logger/graphql-logger';
 export {PolarisGraphQLLogger} from './logger/polaris-graphql-logger';
 export {GraphQLLogProperties} from './logger/graphql-log-properties';
