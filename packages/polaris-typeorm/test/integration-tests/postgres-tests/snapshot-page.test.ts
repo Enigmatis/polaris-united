b5b7e3cbb34524b6a3eaddfbb87494d99d6aaf34
Arik Furman <furmanmail@gmail.com>
2020-08-10 13:19:08 +0300

    test: exctracted before each

--- packages/polaris-typeorm/test/integration-tests/postgres-tests/snapshot-page.test.ts
+++ packages/polaris-typeorm/test/integration-tests/postgres-tests/snapshot-page.test.ts
@@ -1,19 +1,17 @@
 import { PolarisConnection, PolarisRepository, SnapshotPage, SnapshotStatus } from '../../../src';
 import { setUpTestConnection } from '../utils/set-up';
+let connection: PolarisConnection;
+let snapshotRepo: PolarisRepository<SnapshotPage>;
 
+beforeEach(async () => {
+    connection = await setUpTestConnection();
+    snapshotRepo = connection.getRepository(SnapshotPage);
+});
+
+afterEach(async () => {
+    await connection.close();
+});
 describe('snapshot page tests', () => {
-    let connection: PolarisConnection;
-    let snapshotRepo: PolarisRepository<SnapshotPage>;
-
-    beforeEach(async () => {
-        connection = await setUpTestConnection();
-        snapshotRepo = connection.getRepository(SnapshotPage);
-    });
-
-    afterEach(async () => {
-        await connection.close();
-    });
-
     test('saving snapshot with data, snapshot is saved with requested data', async () => {
         const data: string = 'foo';
         const snapshotPage: SnapshotPage = new SnapshotPage('c9895be8-f2c1-4a0b-a532-33808257761a');
