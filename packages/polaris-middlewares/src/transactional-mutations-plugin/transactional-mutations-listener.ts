29cb3a167c298552e2ef7b9192647dd8b760174b
Enigmatis <>
2020-08-24 11:22:02 +0300

    fix: Merge branch 'development' of https://github.com/Enigmatis/polaris-united into transactional-tests
     Conflicts:
    	packages/polaris-core/test/integration/server/schema/resolvers.ts
    	packages/polaris-core/test/integration/server/utils/logger.ts
    	packages/polaris-core/test/integration/server/utils/snapshot-client.ts
    	packages/polaris-middlewares/test/data-version/data-version-middleware-without-connection.test.ts
    	packages/polaris-middlewares/test/irrelevant-entities/irrelevant-entities-middleware-without-connection.test.ts
    	packages/polaris-typeorm/package-lock.json
    	packages/polaris-typeorm/package.json

--- packages/polaris-middlewares/src/transactional-mutations-plugin/transactional-mutations-listener.ts
+++ packages/polaris-middlewares/src/transactional-mutations-plugin/transactional-mutations-listener.ts
@@ -1,7 +1,13 @@
 import { PolarisGraphQLContext } from '@enigmatis/polaris-common';
 import { PolarisGraphQLLogger } from '@enigmatis/polaris-graphql-logger';
-import {PolarisConnection, QueryRunner} from '@enigmatis/polaris-typeorm';
-import { GraphQLRequestContext, GraphQLRequestListener, GraphQLResponse, ValueOrPromise, WithRequired } from 'apollo-server-plugin-base';
+import { PolarisConnection, QueryRunner } from '@enigmatis/polaris-typeorm';
+import {
+    GraphQLRequestContext,
+    GraphQLRequestListener,
+    GraphQLResponse,
+    ValueOrPromise,
+    WithRequired,
+} from 'apollo-server-plugin-base';
 import {
     LISTENER_COMMITTING_MESSAGE,
     LISTENER_FINISHED_JOB,
@@ -22,42 +28,56 @@ export class TransactionalMutationsListener
     public responseForOperation(
         requestContext: WithRequired<
             GraphQLRequestContext<PolarisGraphQLContext>,
-            'metrics' | 'source' | 'document' | 'operationName' | 'operation' | "response" | "request" | "context" | "cache" | "queryHash" | "errors" | "debug"
-            >,
-    ): ValueOrPromise<GraphQLResponse|null> {
+            | 'metrics'
+            | 'source'
+            | 'document'
+            | 'operationName'
+            | 'operation'
+            | 'response'
+            | 'request'
+            | 'context'
+            | 'cache'
+            | 'queryHash'
+            | 'errors'
+            | 'debug'
+        >,
+    ): ValueOrPromise<GraphQLResponse | null> {
         this.queryRunner = this.connection.createQueryRunner();
-        Object.assign(this.queryRunner, {...this.queryRunner, name: requestContext.context.requestHeaders.requestId!});
-        this.connection.addQueryRunner(requestContext.context.requestHeaders.requestId!, this.queryRunner);
+        Object.assign(this.queryRunner, {
+            ...this.queryRunner,
+            name: requestContext.context.requestHeaders.requestId!,
+        });
+        this.connection.addQueryRunner(
+            requestContext.context.requestHeaders.requestId!,
+            this.queryRunner,
+        );
         return null;
     }
-
 
     public willSendResponse(
         requestContext: GraphQLRequestContext<PolarisGraphQLContext> &
             Required<Pick<GraphQLRequestContext<PolarisGraphQLContext>, 'metrics' | 'response'>>,
     ): Promise<void> {
-        const shouldRollback = !!((requestContext.errors && requestContext.errors?.length > 0) ||
-            (requestContext.response.errors && requestContext.response.errors?.length > 0));
+        const shouldRollback = !!(
+            (requestContext.errors && requestContext.errors?.length > 0) ||
+            (requestContext.response.errors && requestContext.response.errors?.length > 0)
+        );
         return this.finishTransaction(requestContext.context, shouldRollback);
     }
     private async finishTransaction(context: PolarisGraphQLContext, shouldRollback: boolean) {
         if (this.queryRunner?.isTransactionActive) {
             if (shouldRollback) {
-                await this.rollbackTransaction(context);
+                await this.queryRunner?.rollbackTransaction();
+                this.logger.warn(LISTENER_ROLLING_BACK_MESSAGE, context);
             } else {
-                await this.commitTransaction(context)
+                await this.queryRunner?.commitTransaction();
+                this.logger.debug(LISTENER_COMMITTING_MESSAGE, context);
             }
         }
-        await this.queryRunner?.release();
+        if (!this.queryRunner?.isReleased) {
+            await this.queryRunner?.release();
+        }
         this.connection.removeQueryRunner(context.requestHeaders.requestId!);
         this.logger.debug(LISTENER_FINISHED_JOB, context);
-    }
-    private async rollbackTransaction(context: PolarisGraphQLContext) {
-        await this.queryRunner?.rollbackTransaction();
-        this.logger.warn(LISTENER_ROLLING_BACK_MESSAGE, context);
-    }
-    private async commitTransaction(context: PolarisGraphQLContext): Promise<void> {
-        await this.queryRunner?.commitTransaction();
-        this.logger.debug(LISTENER_COMMITTING_MESSAGE, context);
     }
 }
