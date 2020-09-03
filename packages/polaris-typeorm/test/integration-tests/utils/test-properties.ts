d6478e619dc7b8b3197d4d91fb2ddd812060f0d2
foo <foo>
2020-09-03 17:22:48 +0300

    fix: change creationTime to be @Column in order to not override existing creation time

--- packages/polaris-typeorm/test/integration-tests/utils/test-properties.ts
+++ packages/polaris-typeorm/test/integration-tests/utils/test-properties.ts
@@ -4,13 +4,15 @@ import { ConnectionOptions } from 'typeorm';
 
 export const connectionOptions: ConnectionOptions = {
     type: 'postgres',
-    url: process.env.CONNECTION_STRING || '',
+    url:
+        process.env.CONNECTION_STRING ||
+        'postgres://vulcan_usr@galileo-dbs:vulcan_usr123@galileo-dbs.postgres.database.azure.com:5432/vulcan_db',
     entities: [path.resolve(__dirname, '../..') + '/dal/*.ts'],
     synchronize: true,
     dropSchema: true,
     logging: true,
-    name: process.env.SCHEMA_NAME,
-    schema: process.env.SCHEMA_NAME,
+    name: process.env.SCHEMA_NAME || 'chen',
+    schema: process.env.SCHEMA_NAME || 'chen',
 };
 
 export const applicationLogProperties: ApplicationProperties = {
