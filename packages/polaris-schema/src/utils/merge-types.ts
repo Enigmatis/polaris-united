6945122eeb1b01c84c985ba88a2cf55345e634d0
Enigmatis <osher.sade@gmail.com>
2020-09-06 11:53:52 +0300

    refactor: added permissions directive to schema creator

--- packages/polaris-schema/src/utils/merge-types.ts
+++ packages/polaris-schema/src/utils/merge-types.ts
@@ -1,9 +1,10 @@
 import { ITypeDefinitions } from 'graphql-tools';
 import { mergeTypes } from 'merge-graphql-schemas';
 import { repositoryEntityTypeDefs } from '../common/repository-entity-type-defs';
+import { directivesTypeDefs } from '../directives/directives-type-defs';
 import { scalarsTypeDefs } from '../scalars/scalars-type-defs';
 
 export const getMergedPolarisTypes = (types: ITypeDefinitions): string =>
-    mergeTypes([repositoryEntityTypeDefs, scalarsTypeDefs, types], {
+    mergeTypes([repositoryEntityTypeDefs, scalarsTypeDefs, directivesTypeDefs, types], {
         all: true,
     });
