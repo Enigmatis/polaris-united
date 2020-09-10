001f1bb45c8c88fe4fef5c9308e9f35375d59245
chenshoo <chenshooker@gmail.com>
2020-09-10 16:05:40 +0300

    fix: fixed permission directive definition (#13)
    * fix: fixed permission directive definition
    
    Co-authored-by: Enigmatis <osher.sade@gmail.com>

---
+++ packages/polaris-core/test/integration/tests/permissions.test.ts
@@ -0,0 +1,34 @@
+import http from 'http';
+import { PolarisServer } from '../../../src';
+import {
+    startPermissionServer,
+    stopPermissionServer,
+} from '../permission-server-mock/permission-server';
+import { startTestServer, stopTestServer } from '../server/test-server';
+import { graphQLRequest } from '../server/utils/graphql-client';
+
+let polarisServer: PolarisServer;
+let permissionServer: http.Server;
+
+beforeEach(async () => {
+    permissionServer = await startPermissionServer();
+    polarisServer = await startTestServer();
+});
+
+afterEach(async () => {
+    await stopPermissionServer(permissionServer);
+    return stopTestServer(polarisServer);
+});
+
+describe('permissions tests', () => {
+    it('query with authorized upn', async () => {
+        const headers = { 'oicd-claim-upn': '123' };
+        const result = await graphQLRequest('{ permissionsField }', headers);
+        expect(result.permissionsField).toBe('foo bar baz');
+    });
+
+    it('query with unauthorized upn', async () => {
+        const headers = { 'oicd-claim-upn': '321' };
+        await expect(graphQLRequest('{ permissionsField }', headers)).rejects.toThrow('Forbidden');
+    });
+});
