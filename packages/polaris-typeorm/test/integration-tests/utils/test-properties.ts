bbbd7a7d05ed8216de68446794b78140ff2d0118
osher-sade <osher.sade@gmail.com>
2020-10-29 13:29:33 +0200

    ci: fixed travis timing out

--- packages/polaris-typeorm/test/integration-tests/utils/test-properties.ts
+++ packages/polaris-typeorm/test/integration-tests/utils/test-properties.ts
@@ -22,7 +22,7 @@ export const applicationLogProperties: ApplicationProperties = {
 };
 
 export const loggerConfig: LoggerConfiguration = {
-    loggerLevel: LoggerLevel.INFO,
+    loggerLevel: LoggerLevel.WARN,
     writeToConsole: true,
     writeFullMessageToConsole: false,
 };
