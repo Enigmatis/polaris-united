6d97ad3d4ab2431637752c37de9911fc0add8ca5
osher-sade <osher.sade@gmail.com>
2020-11-05 17:16:56 +0200

    fix: permissions service wrapper will now produce clearer errors

--- packages/polaris-permissions/src/permissions-service-wrapper.ts
+++ packages/polaris-permissions/src/permissions-service-wrapper.ts
@@ -73,16 +73,25 @@ export class PermissionsServiceWrapper {
         }
 
         if (!this.permissionsCacheHolder.isCached(entityType)) {
-            const permissionResponse = await this.sendRequestToExternalService(
-                requestUrl,
-                proxy,
-                permissionHeaders,
-            );
+            let permissionResponse;
+            try {
+                permissionResponse = await this.sendRequestToExternalService(
+                    requestUrl,
+                    proxy,
+                    permissionHeaders,
+                );
+            } catch (e) {
+                if (e.response) {
+                    permissionResponse = e.response;
+                } else {
+                    throw new Error(
+                        'Unexpected error occurred when tried to access external permissions service',
+                    );
+                }
+            }
 
             if (permissionResponse.status !== 200) {
-                throw new Error(
-                    `Status response ${permissionResponse.status} is received when access external permissions service`,
-                );
+                throw new Error(permissionResponse.data.error);
             }
 
             this.permissionsCacheHolder.addCachedHeaders(entityType, permissionResponse.headers);
