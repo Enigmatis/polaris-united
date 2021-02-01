3da2857817f3ef72c75f01b29f0ca2e293a1cdd6
Ron Katz <ron.katzzz@gmail.com>
2021-02-01 13:39:54 +0200

    feat: before tests

--- packages/polaris-test/server/foo.ts
+++
@@ -1,7 +0,0 @@
-import { startTestServer } from './test-server';
-
-startTestServer()
-    // tslint:disable-next-line:no-console
-    .then((r) => console.log('server started'))
-    // tslint:disable-next-line:no-console
-    .catch((e) => console.log('R.I.P because of ' + e));
